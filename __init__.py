439372a5ccf73a4c1f0a08ed21fa0589bca58935
bboytian <94tian@gmail.com>
2020-09-14 11:47:39 +0800

    webswitch_reader/ implemented

--- __init__.py
+++ __init__.py
@@ -1,3 +1,5 @@
 from .mpl_reader import smmpl_reader, mpl_reader
 from .mpl_reader.smmpl_fmt import import_dic as smmplfmt_dic
 from .mpl_reader.mpl_fmt import import_dic as mplfmt_dic
+
+from .webswitch_reader import main as webswitch_reader
