d3ba82a81271b48bde26819d7ac814cb8d30f61f
bboytian <94tian@gmail.com>
2020-09-15 14:28:11 +0800

    webswitch_reader/ bugfix

--- webswitch_reader/__main__.py
+++ webswitch_reader/__main__.py
@@ -43,7 +43,11 @@ def main(
         raise ValueError('either filedir or text must be specified')
 
     # removing header
-    lines_a = list(filter(lambda x: x != '', lines_a))  # remove empty lines
+    lines_a = list(filter(
+        lambda x: x != '' and 'xx/xx/xxxx xx:xx:xx' not in x,
+        lines_a
+    ))  # remove empty lines
+
     lines_a = lines_a[_headersize:]
 
     # parsing lines
