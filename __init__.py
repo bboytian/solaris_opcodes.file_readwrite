7cf6eb169f429476fb8eb4d9a75f138d304a3e99
bboytian <94tian@gmail.com>
2021-02-24 19:47:36 +0800

    updated __init__ to include cloudheigh_reader

--- __init__.py
+++ __init__.py
@@ -5,3 +5,5 @@ from .mpl_reader.mpl_fmt import import_dic as mplfmt_dic
 from .webswitch_reader import main as webswitch_reader
 
 from .datajson_readwrite import datajson_write, datajson_read
+
+from .cloudproduct_reader import main as cloudproduct_reader
